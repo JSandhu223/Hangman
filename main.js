a4168ef5fef32613656e81c344ea3ba15691a55a
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2023-10-11 20:50:56 -0600

    changed placeholder character from # to _

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createLetterPlaceholders(selectedWord) {
     for (let i = 0; i < selectedWord.length; i++) {
         let letterSpan = document.createElement("span");
         letterSpan.className = "letter";
-        letterSpan.textContent = "#";
+        letterSpan.textContent = "_";
         wordDisplay.appendChild(letterSpan);
     }
 }
