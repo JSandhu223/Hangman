cd494e5065a790a6cf5d1f8bd2356acca472a344
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2023-10-24 01:50:07 -0600

    clicking PLAY now redirects to game page.

--- event-handlers.js
+++ event-handlers.js
@@ -1,3 +1,12 @@
+// Handle the user clicking PLAY on main menu
+function handlePlayClick() {
+    window.location.href = "game.html";
+}
+
+// Add event listener to PLAY button on main menu
+let play = document.getElementById("play-button");
+play.addEventListener("click", handlePlayClick);
+
 // Handle the user clicking on a floating letter
 function handleFloatingLetterClick() {
     alert("Floating letter clicked");
