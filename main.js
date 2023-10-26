cee13e2007bf8ff0b55054a58bc4b19f5abd757c
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2023-10-26 16:37:07 -0600

    deleted event handlers file. Moved event handlers back to main.js

--- main.js
+++ main.js
@@ -101,5 +101,23 @@ function startNewGame() {
     spawnFloatingLetter();
 }
 
-// Call this when the user clicks button to start a new game.
-startNewGame();
+
+//////////////////////// Event Handling ///////////////////////////////////
+
+window.addEventListener("load", function () {
+    startNewGame();
+})
+
+// // Handle the user clicking PLAY on main menu
+// function handlePlayClick() {
+//     window.location.href = "game.html";
+// }
+
+// // Add event listener to PLAY button on main menu
+// let play = document.getElementById("play-button");
+// play.addEventListener("click", handlePlayClick);
+
+// Handle the user clicking on a floating letter
+function handleFloatingLetterClick() {
+    alert("Floating letter clicked");
+}
